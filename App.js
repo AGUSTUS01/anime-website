238530d1dca2632ad108dd175f5d5c714869c255
NAYEEM <112334233+AGUSTUS01@users.noreply.github.com>
2023-04-01 03:24:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Greet from './components/Greet';
+import Message from './components/message';
+class App extends Component{ 
+  render(){ 
+  return (
+    <div>
+      < Message/>
+      
+    </div>
+  );
+}
+}
+
+export default App;
